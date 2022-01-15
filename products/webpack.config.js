982aac635649acc63d72be16a26ddedd29925356
prakhargvp <p.agarwal@crif.com>
2022-01-15 16:52:20 +0530

    Implementing Module Federation

--- products/webpack.config.js
+++ products/webpack.config.js
@@ -1,11 +1,18 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-
+const ModulefederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 module.exports = {
     mode: 'development',
     devServer: {
         port: 8081,
     },
     plugins: [
+        new ModulefederationPlugin({
+            name: 'products',
+            filename: 'remoteEntry.js',
+            exposes: {
+                './ProductIndex': './src/index'
+            }
+        }),
         new HtmlWebpackPlugin({
             template: './public/index.html',
         })
